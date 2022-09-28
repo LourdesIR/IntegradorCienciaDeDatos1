fc888e880eb3d3f05cdde5c896a226853eae63ae
LourdesIR <lourdes.reynaldo@gmail.com>
2022-09-28 20:21:38 -0300

    Se agrega file inicial

---
+++ formulas.py
@@ -0,0 +1,18 @@
+# 1 - suma, resta, producto y división de 2 numeros con impresion de pantalla.
+
+# 2 - el mayor de 3 numeros enteros ingresados por el usuario. mostrar resultado y los 3 numeros por pantalla.
+
+# 3 - Cantidad a cobrar del cliente.  Validar ingreso del mes de acuerdo a un ingreso. 
+# Discriminar en las impresiones de pantalla los valores referidos al total, descuento e importe a pagar final. 
+
+# 4 - Codigo de calificaciones docentes. validacion de notas.
+
+# 5 - Programa que escriba hola 5 veces.
+
+# 6 - escribir un numero y obtener los numeros pares menor a este.
+
+# 7 - Pedir un numero y devolver el mes. Validar ingreso.
+
+# 8 - Suma y promedio de 10 valores.
+
+# 9 - Imprimir la suma de los números impares del 1 al 25.
